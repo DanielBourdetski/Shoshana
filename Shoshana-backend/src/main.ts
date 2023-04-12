c3db5b0de8b4866e35399f9a51010a73a6d50d97
TopKekHam <danielriz@protonmail.com>
2023-04-12 19:48:49 +0300

    Update main.ts

--- Shoshana-backend/src/main.ts
+++ Shoshana-backend/src/main.ts
@@ -7,7 +7,6 @@ import { JWTToken, UserType } from "./types";
 const app = Express();
 const port = 3000
 
-<<<<<<< HEAD
 loadDB("mongodb://localhost:27017").catch(console.dir);
 
 app.get('/login', (req, res) => {
@@ -17,11 +16,6 @@ app.get('/login', (req, res) => {
 })
 
 app.get('/', createAuthMiddleware([UserType.Admin]) ,(req, res) => {
-=======
-loadDB("mongodb://127.0.0.1:27017").catch(console.dir);
-``
-app.get('/', (req, res) => {
->>>>>>> cf634d1eaa853ff5faac13b1848cf8619d59c9e5
     res.send('Hello World!')
 })
 
