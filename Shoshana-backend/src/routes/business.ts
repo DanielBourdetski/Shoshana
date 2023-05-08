da8e0ed6ed55509ec61d9e87257a38f13a117e8c
TopKekHam <danielriz@protonmail.com>
2023-05-08 17:46:30 +0300

    formating

--- Shoshana-backend/src/routes/business.ts
+++ Shoshana-backend/src/routes/business.ts
@@ -1,4 +1,4 @@
-import {Router} from "express"
+import { Router } from "express";
 import { createAuthMiddleware } from "../middleware/auth";
 import { UserType } from "../types";
 
@@ -8,19 +8,17 @@ const authBusiness = createAuthMiddleware([UserType.Business, UserType.Admin]);
 
 // read
 router.get("/business/[id]", (req, res) => {
-    
+
 });
 
-router.post("/businesss", (req, res) => {
+router.get("/business", (req, res) => {
 
 })
 
-// write
-router.post('/business/[id]', (req, res) => {
+router.post("/businesss", (req, res) => {});
 
-});
+// write
+router.post("/business/[id]", (req, res) => {});
 
 // read business icon
-router.get("/business/icon/[id]", (req, res) => {
-
-});
+router.get("/business/icon/[id]", (req, res) => {});
