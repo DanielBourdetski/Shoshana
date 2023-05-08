ebccbb8f80de44da448e04247939929ebff211f6
TopKekHam <danielriz@protonmail.com>
2023-05-08 17:46:17 +0300

    Business and appointments added to db

--- Shoshana-backend/src/database.ts
+++ Shoshana-backend/src/database.ts
@@ -1,94 +1,128 @@
 import { Option, none, some, ResolvedPromise } from "topkek-utils";
-import {
-  Document,
-  MongoClient,
-  ObjectId,
-  ServerApiVersion,
-  WithId,
-  WithoutId,
-} from "mongodb";
-import { Appointment, Business, UserType } from "./types";
-
-export type User = WithId<{
-  username: string;
-  password: string;
-  userType: UserType
-}>;
+import { MongoClient, ObjectId, WithoutId } from "mongodb";
+import { Appointment, Business, User, UserType } from "./types";
 
 let dbName = "test";
 let collectionUsers = "users";
 let collectionBusinesses = "businesses";
+let collectionImages = "images";
 
 let client: MongoClient;
 
 export async function loadDB(url: string): Promise<void> {
-  //console.log("connecting to mongoDB");
+	//console.log("connecting to mongoDB");
 
-  client = new MongoClient(url);
+	client = new MongoClient(url);
 
-  try {
-    // Connect the client to the server (optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
-  } finally {
-    // Ensures that the client will close when you finish/error
-    //await client.close();
-  }
+	try {
+		// Connect the client to the server (optional starting in v4.7)
+		await client.connect();
+		// Send a ping to confirm a successful connection
+		await client.db("admin").command({ ping: 1 });
+		console.log(
+			"Pinged your deployment. You successfully connected to MongoDB!"
+		);
+	} finally {
+		// Ensures that the client will close when you finish/error
+		//await client.close();
+	}
 }
 
 export async function getUserByUsername(
-  username: string
+	username: string
 ): ResolvedPromise<Option<User>> {
-  let res = await (client
-    .db(dbName)
-    .collection(collectionUsers)
-    .findOne({ username }) as Promise<User | null>);
-  return res ? some(res) : none(undefined);
+	let res = await (client
+		.db(dbName)
+		.collection(collectionUsers)
+		.findOne({ username }) as Promise<User | null>);
+	return res ? some(res) : none(undefined);
 }
 
 export async function registerUser(
-  username: string,
-  password: string,
-  userType: UserType
+	username: string,
+	password: string,
+	userType: UserType
 ): ResolvedPromise<Option<ObjectId>> {
-  //cheching if user with same username already exists
-  let existingUser = await getUserByUsername(username);
+	//cheching if user with same username already exists
+	let existingUser = await getUserByUsername(username);
 
-  if (existingUser.ok) return none(undefined);
+	if (existingUser.ok) return none(undefined);
 
-  // inserting new user
-  let res = await client
-    .db(dbName)
-    .collection(collectionUsers)
-    .insertOne({ username, password, userType });
+	// inserting new user
+	let res = await client
+		.db(dbName)
+		.collection(collectionUsers)
+		.insertOne({ username, password, userType });
 
-  return some(res.insertedId);
+	return some(res.insertedId);
 }
 
-// export async function createBusiness(userId: ObjectId, name: string): ResolvedPromise<Option<ObjectId>> {
-//   let business: Business = {
-//     userId: userId,
-//     name,
-//     appoinments: {},
-//   };
+export async function deleteUser(userId: ObjectId): ResolvedPromise<boolean> {
+	let res = await client
+		.db(dbName)
+		.collection(collectionUsers)
+		.deleteOne({ _id: userId });
 
-//   let res = await client
-//     .db(dbName)
-//     .collection(collectionBusinesses)
-//     .insertOne(business);
+	return res.acknowledged && res.deletedCount === 1;
+}
 
-//   if (res.acknowledged === false) return none(undefined);
+export async function getBussinessByOwnerId(
+	id: ObjectId
+): ResolvedPromise<Option<Business>> {
+	let res = await client
+		.db(dbName)
+		.collection(collectionBusinesses)
+		.findOne({ ownerId: id }, { projection: { appointments: 0 } });
 
-//   return some(res.insertedId);
-// }
+	if (res != null) {
+		return some(res as Business);
+	} else {
+		return none(undefined);
+	}
+}
+
+export async function createBusiness(
+	business: WithoutId<Business>
+): ResolvedPromise<Option<ObjectId>> {
+	let res = await client
+		.db(dbName)
+		.collection(collectionBusinesses)
+		.insertOne(business);
+
+	if (res.acknowledged) {
+		return some(res.insertedId);
+	} else {
+		return none(undefined);
+	}
+}
+
+export async function createImage(
+	imageData: Uint8Array
+): ResolvedPromise<Option<ObjectId>> {
+	let res = await client
+		.db(dbName)
+		.collection(collectionImages)
+		.insertOne({ data: imageData });
+
+	if (res.acknowledged) {
+		return some(res.insertedId);
+	} else {
+		return none(undefined);
+	}
+}
+
+export async function deleteImage(imageId: ObjectId): ResolvedPromise<boolean> {
+	let res = await client
+		.db(dbName)
+		.collection(collectionImages)
+		.deleteOne({ _id: imageId });
+
+	return res.acknowledged && res.deletedCount === 1;
+}
 
 // export async function createAppointment(businessId : ObjectId, appointment : WithoutId<Appointment>) : ResolvedPromise<Option<ObjectId>>
 // {
-  
+
 //   let field = appointment.date.year + "_" + appointment.date.month;
 
 //   let res = await client.db(dbName).collection(collectionBusinesses)
--- Shoshana-backend/src/types.ts
+++ Shoshana-backend/src/types.ts
@@ -1,34 +1,53 @@
-import { ObjectId } from "mongodb";
+import { ObjectId, WithId } from "mongodb";
 
 export enum UserType {
-    Admin = 69,
-    Business = 420
+	Admin = 69,
+	Business = 420,
 }
 
 export type JWTToken = {
-    userId: string;
-    userType: UserType;
-}
+	userId: string;
+	businessId: string;
+	userType: UserType;
+};
+
+export type User = WithId<{
+	username: string;
+	password: string;
+	email: string;
+	name: string;
+	userType: UserType;
+}>;
 
 export type AppointmentDate = {
-    year: number;
-    month: number;
-    day: number;
-    hour: number;
-    minute: number;
-    hourDiff: number;
-    minuteDiff: number;
-}
+	year: number;
+	month: number;
+	day: number;
+	hour: number;
+	minute: number;
+	hourDiff: number;
+	minuteDiff: number;
+};
 
 export type Appointment = {
-    title: string;
-    description: string;
-    author: string;
-    date: AppointmentDate;
-}
+	title: string;
+	description: string;
+	author: string;
+	date: AppointmentDate;
+};
 
-export type Business = {
-    userId : ObjectId;
-    name : string;
-    appoinments : Appointment[];
-}
+export type Business = WithId<{
+	ownerId: ObjectId;
+	businessName: string;
+	address: string;
+	phoneNumber: {
+		private: string;
+		public: string;
+	};
+	appointments: Appointment[];
+	logo: ObjectId;
+}>;
+
+export type Image = WithId<{
+	data: Uint8Array;
+}>;
