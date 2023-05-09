3063af6f841e8f403f5e733ecf0658c9b2e66e12
DanielBourdetski <69850165+DanielBourdetski@users.noreply.github.com>
2023-05-09 15:37:43 +0300

    added Toaster singleton

---
+++ Shoshana-front/src/helpers/toaster.tsx
@@ -0,0 +1,46 @@
+import { toast } from "react-toastify";
+
+const dftStyle = {
+  padding: 10,
+  textAlign: "center",
+};
+
+class Toaster {
+  private static instance: Toaster;
+
+  private constructor() {}
+
+  public static getInstance(): Toaster {
+    if (!Toaster.instance) {
+      Toaster.instance = new Toaster();
+    }
+
+    return Toaster.instance;
+  }
+
+  public authError(err: string): void {
+    toast.error(err, {
+      autoClose: 2000,
+      hideProgressBar: true,
+      closeButton: false,
+      style: {
+        textAlign: "center",
+        padding: 10,
+        color: "#444",
+        border: "1px solid red",
+      },
+    });
+  }
+
+  public info(msg: string): void {
+    toast.info(msg, {
+      autoClose: 2000,
+      style: {
+        textAlign: "center",
+        padding: 10,
+      },
+    });
+  }
+}
+
+export default Toaster.getInstance();
