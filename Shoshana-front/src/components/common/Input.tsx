0104df4152689648e4bdad6f4a50885fd26acaf2
Olga Shaburd <Daniel.Bourdetski@gmail.com>
2023-05-09 14:31:59 +0300

    added Input component

---
+++ Shoshana-front/src/components/common/Input.tsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+
+type InputProps = {
+  className?: string;
+  onChange: Function;
+  value: string;
+  label: string;
+  password?: boolean;
+  error?: string;
+};
+
+const Input: React.FC<InputProps> = ({
+  className,
+  onChange,
+  value,
+  label,
+  password = false,
+  error,
+}) => {
+  const [inputIsFocused, setInputIsFocused] = useState(false);
+
+  const activeLabelClassName = "transform -translate-y-10 text-xs italic";
+
+  return (
+    <div className="flex flex-col items-center justify-center relative mb-10 ">
+      <div className="relative ">
+        <label
+          htmlFor={label}
+          className={`absolute cursor-text duration-150 left-2 top-1/2 -translate-y-1/2 text-slate-500 pointer-events-none ${
+            value || inputIsFocused ? activeLabelClassName : ""
+          }`}
+        >
+          {label}
+        </label>
+        <input
+          className={`h-10 w-60 rounded-t border-b border-b-blue-400 bg-blue-50 outline-none p-2 ps-4 focus:bg-blue-100 focus:border-b-2 duration-300 ease-in-out ${
+            error && "border-b-red-500 bg-red-50 focus:bg-red-100"
+          }`}
+          id={label}
+          type={password ? "password" : "text"}
+          onFocus={() => setInputIsFocused(true)}
+          onBlur={() => (value ? null : setInputIsFocused(false))}
+          onChange={(e) => onChange(e)}
+          value={value}
+        />
+      </div>
+      {error && <div className="text-red-600 w-full text-sm">{error}</div>}
+    </div>
+  );
+};
+
+export default Input;
