3bb3c53f3bb9946b6cd6d764583352f46fd23a34
Olga Shaburd <Daniel.Bourdetski@gmail.com>
2023-05-09 14:28:13 +0300

    refactor and bug fix

--- Shoshana-front/src/components/AuthGuard.tsx
+++ Shoshana-front/src/components/AuthGuard.tsx
@@ -1,22 +1,22 @@
-import React, { useContext, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import authService from '../services/authService';
+import React, { useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import authService from "../services/authService";
 
 interface Props {
-	Component: () => JSX.Element;
-	elementType?: string;
+  Component: () => JSX.Element;
+  elementType?: string;
 }
 
 const AuthGuard = ({ Component }: Props) => {
-	const navigate = useNavigate();
-	
-	useEffect(()=> {
-		if(!authService.isLoggedIn()) {
-			navigate("/");
-		}
-	}, []);
+  const navigate = useNavigate();
 
-	return <Component />;
+  useEffect(() => {
+    if (!authService.isLoggedIn()) {
+      navigate("/auth/login");
+    }
+  }, []);
+
+  return <Component />;
 };
 
 export default AuthGuard;
