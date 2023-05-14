5797ce0ff0d99147b9a9eabeead081f773c26053
DanielBourdetski <69850165+DanielBourdetski@users.noreply.github.com>
2023-05-14 14:18:03 +0300

    Update main.ts

--- Shoshana-backend/src/main.ts
+++ Shoshana-backend/src/main.ts
@@ -17,9 +17,35 @@ app.use("/test", TESTRouter);
 
 mongoose
 	.connect("mongodb://localhost:27017/shoshana")
-	.then(() => console.log("Connected to MognoDB"))
+	.then(() => console.log("mongo: Connected to MognoDB"))
 	.catch(() => console.error("MongoDB connection error"));
 
-app.listen(port, () => {
+app.listen(port, async () => {
 	console.log(`Successfuly connected to http://localhost:${port}`);
+	console.log("server: Admin user check");
+
+	let user = (await Business.exists({ username: "admin" })) as any;
+
+	if (!user) {
+		user = new Business({
+			username: "admin",
+
+			password: "admin",
+			email: "admin@shoshana.com",
+			name: {
+				first: "admin",
+				last: "admin",
+			},
+			businessName: "Shoshana LLC",
+			address: "some magical address",
+			phoneNumbers: { public: "0545454545", private: "0545454545" },
+			logo: "SHOSHANA LOGO",
+		});
+
+		await user.save();
+
+		return console.log("server: Admin created!");
+	}
+
+	console.log("server: Admin exists");
 });
