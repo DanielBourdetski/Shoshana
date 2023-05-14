ddd85b5b386c4dfdda5de46ac2829b1ad936d359
DanielBourdetski <69850165+DanielBourdetski@users.noreply.github.com>
2023-05-14 13:20:21 +0300

    delegated StartingPage logic to Registration and Login components

--- Shoshana-front/src/components/StartingPage.tsx
+++ Shoshana-front/src/components/StartingPage.tsx
@@ -7,9 +7,6 @@ import Login from "./auth/Login";
 import Registration from "./auth/registration/Registration";
 
 const StartingPage = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-
   const navigate = useNavigate();
   const formType = useParams().formType as "login" | "register";
 
@@ -20,21 +17,6 @@ const StartingPage = () => {
   useEffect(() => {
     if (userIsLoggedIn) navigate("/manager");
   }, []);
-
-  const onLogin = async () => {
-    const userData = await authService.login(username, password);
-
-    navigate("/manager");
-  };
-
-  const onRegister = async () => {
-    const userData = await authService.register(username, password);
-    console.log(userData);
-
-    // authService.register({username, password})
-
-    navigate("/manager");
-  };
 
   return (
     <div className="w-full h-full flex flex-col items-center justify-center">
