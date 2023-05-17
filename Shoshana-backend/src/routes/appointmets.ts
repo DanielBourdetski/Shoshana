ee89401df042bb05444ccfdb9bad0d99366a87cf
DanielBourdetski <69850165+DanielBourdetski@users.noreply.github.com>
2023-05-18 00:11:49 +0300

    working on appointments route

--- Shoshana-backend/src/routes/appointmets.ts
+++ Shoshana-backend/src/routes/appointmets.ts
@@ -1,22 +1,53 @@
-import { Router } from "express";
+import { Request, Router } from "express";
+import { auth } from "../middleware/auth";
+import { Business } from "../types";
+import Appointment, { IAppointment } from "../models/appointment";
 const router = Router();
 
+type ExtendedRequest = Request & {
+	business?: Business; // Add the business property to the ExtendedRequest type
+};
+
 // ? get all appointments of logged business /
-router.get("/", (req, res) => {});
+router.get("/", auth, (req: ExtendedRequest, res) => {
+	const apts = req.business?.appointments;
+
+	if (!apts || !apts.length) return res.send("No Appointments");
+
+	res.json(req.business?.appointments);
+});
 
 // ? get certain appointment
-router.get("/:id", (req, res) => {});
+router.get("/:id", (req: ExtendedRequest, res) => {
+	const apts = req.business?.appointments;
 
-// ? set new appointment
-router.post("/", (req, res) => {});
+	if (!apts || !apts.length) return res.send("No Appointments");
+
+	console.log(apts[0]);
+	res.json(apts);
+});
+
+// ? create new appointment
+router.post("/", auth, async (req: ExtendedRequest, res) => {
+	const apt = req.body;
+
+	const newApt = new Appointment({
+		...apt,
+		businessId: req.business?._id.toString(),
+	});
+	console.log("apt:", newApt);
+
+	await newApt.save();
+	res.json(newApt);
+});
 
 // ? update appointment
-router.put("/:id", (req, res) => {});
+router.put("/:id", (req: ExtendedRequest, res) => {});
 
 // ? cancel an appointment, will leave it in the database
-router.delete("/:id", (req, res) => {});
+router.delete("/:id", (req: ExtendedRequest, res) => {});
 
 // ? delete an appointment completely
-router.delete("/perma/:id", (req, res) => {});
+router.delete("/perma/:id", (req: ExtendedRequest, res) => {});
 
 export default router;
