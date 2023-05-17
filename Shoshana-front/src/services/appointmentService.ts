6090c2fa13c7b4fc478e8b29ce238073a3e0c63a
DanielBourdetski <69850165+DanielBourdetski@users.noreply.github.com>
2023-05-18 00:10:23 +0300

    created shell for appointment service

---
+++ Shoshana-front/src/services/appointmentService.ts
@@ -0,0 +1,25 @@
+import httpService from "./httpService";
+
+export const addAppointment = async ({}) => {};
+
+export const updateAppointment = async () => {};
+
+export const getAppointmentById = async () => {};
+
+export const getAppointmentsByDate = async () => {};
+
+export const getAppointmentsByDateRange = async () => {};
+
+export const cancelAppointment = async () => {};
+
+export const deleteAppointment = async () => {};
+
+export default {
+  addAppointment,
+  updateAppointment,
+  getAppointmentById,
+  getAppointmentsByDate,
+  getAppointmentsByDateRange,
+  cancelAppointment,
+  deleteAppointment,
+};
